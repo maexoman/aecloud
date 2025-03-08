415773e7c27e410b03fe66e5bfde88a6f59a4aef
Maximilian Heinrich <mex.heinrich@gmail.com>
2025-03-08 23:14:41 +0100

    feat(image-creation): add tool to generate new vm images (#1)
    * docs(vms/image-creation): add motivation
    
    * docs(vms/image-creation): fix typo
    
    * docs(vms/image-creation): add motivation behind not using Packer
    
    * feat(vms/image-creation): add initial setup
    
    * fix(vms/image-creation): allow aecloud-admin user to use sudo without a password (probably unsafe)
    
    * feat(vms/image-creation): add some configuration options
    
    * docs(vms/image-creation): how to use the tool
    
    * fix(vms/image-creation): clean up the public key to remove comment

---
+++ vms/01-image-creation/README.md
@@ -0,0 +1,43 @@
+# Image Creation
+
+## Motivation
+I would like to minimize the manual labor required to set up new vms.\
+Previously I used to manually install the Ubuntu ISO into a new qcow2 vm image drive providing a user and random password.\
+Afterwards I let Ansible Scripts run to upgrade the system, add a new user, setting authorized keys and hardening the system.\
+I've learned even cloud providers like AWS seem to use pre-installed base images. When starting a newly created VM something like cloud-init doing some of the things I used ansible for.\
+First I would need to create a base image. I also want to automate this step so no more manual labor is required :)\
+As I've learned this can be used with autoinstall.\
+\
+I know that there are applications like hashicorps packer to provide abstrations making this easier.\
+I would like to try creating a similar but more basic version myself. This way I get to learn how Packer works under the hood.
+
+## Requirements
+Packages required for this module:
+- genisoimage
+
+Files required for this module:
+- ISO for installation (it has only been tested with Ubuntu Server 24.04.2)
+
+## Usage
+You'll have to provide a valid public-key-path and an iso path to create an image.\
+You can also provide a hostname and username if you prefer.
+
+```bash
+# the "simples" command:
+./create-bash-image.sh \
+    --public-key-file ./path/to/key.pub \
+    --iso ./path/to/ubuntu.iso
+```
+
+Or just see what else you can change:
+```bash
+./create-bash-image.sh --help
+```
+
+Please use sensible values as they will not be validated yet.
+
+## How does it work?
+This module uses the autoinstall provided by Ubuntu. The configuration file is needed inside a seperate ISO which will also be provided to QEMU when installing.\
+Unfortunetly I needed to read the files `vmlinuz` and `initrd` from the original Ubuntu ISO to be able to add the autoinstall argument as a kernel arg.\
+These two files will temporarily be copied to the working directory stored in the out folder.\
+Both files will be provided to qemu and the `autoinstall` will be appended.
---
+++ vms/01-image-creation/create-base-image.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# provide sensible defaults:
+HOSTNAME=aecloud
+USERNAME=aecloud-admin
+
+IMAGE_SIZE=5G
+
+IMAGE_OUT=image.qcow2
+
+# parse the arguments provided to the script
+while [[ $# -gt 0 ]]; do
+  case $1 in
+	-h|--help)
+	  echo "here are the allowed options:"
+	  echo " --public-key-file >> path to the public key file"
+	  echo " --iso >> path to the ISO file"
+	  echo " --hostname >> hostname for the new machine"
+	  echo " --username >> username for the new machine"
+	  echo " -o,--out >> path on where to store the image"
+	  echo " --image-size >> required size of the image"
+	  echo ""
+	  echo " --public-key-file and --iso are required"
+	  exit 0
+	  ;;
+	--public-key-file)
+	  PUBLIC_KEY_FILE="$2"
+	  shift # past argument
+	  shift # past value
+	  ;;
+	--iso)
+	  ISO_PATH="$2"
+	  shift # past argument
+	  shift # past value
+	  ;;
+	--hostname)
+	  HOSTNAME="$2"
+	  shift # past argument
+	  shift # past value
+	  ;;
+	--username)
+	  USERNAME="$2"
+	  shift # past argument
+	  shift # past value
+	  ;;
+	-o|--out)
+	  IMAGE_OUT="$2"
+	  shift # past argument
+	  shift # past value
+	  ;;
+	--image-size)
+	  IMAGE_SIZE="$2"
+	  shift # past argument
+	  shift # past value
+	  ;;
+
+	-*|--*)
+	  echo "Unknown option $1"
+	  exit 1
+	  ;;
+	*)
+	  echo "Unknown option $1"
+	  exit 1
+	  ;;
+  esac
+done
+
+# Check the required variables
+if [ -z "${PUBLIC_KEY_FILE}" ]; then
+	echo "Please provide the --public-key-file"
+	exit 1
+fi
+if [ -z "${ISO_PATH}" ]; then
+	echo "Please provide the --iso-path"
+	exit 1
+fi
+
+# validation would be awesome.... :D
+UNCLEANED_PUBLIC_KEY=$(cat $PUBLIC_KEY_FILE)
+PUBLIC_KEY=$(echo "$UNCLEANED_PUBLIC_KEY" | awk '{print $1, $2}')
+
+TEMP_DIR_RANDOM=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 4; echo)
+TEMP_DIR=$(echo "temp-$TEMP_DIR_RANDOM")
+
+# cleanup if any previous images have been created
+if [ -d "$TEMP_DIR" ]; then
+	rm -rf $TEMP_DIR
+fi
+
+# setup working dir structure
+mkdir -p $TEMP_DIR/workdir
+mkdir -p $TEMP_DIR/workdir/seed # used to create the seed.iso
+mkdir -p $TEMP_DIR/workdir/iso-mount # mount point to copy vmlinuz and initrd
+mkdir -p $TEMP_DIR/workdir/iso-copies # use to temporarily save vmlinuz and initrd
+
+# create the iso with the autoinstall
+touch $TEMP_DIR/workdir/seed/meta-data
+cp autoinstall.template.yml $TEMP_DIR/workdir/seed/user-data
+
+# replace the template strings
+# the public key might contain a / meaning it would end the basic sed s/foo/bar/ -> use # instead
+sed -i -e "s#__HOSTNAME__#$HOSTNAME#" $TEMP_DIR/workdir/seed/user-data
+sed -i -e "s#__USERNAME__#$USERNAME#" $TEMP_DIR/workdir/seed/user-data
+sed -i -e "s#__PUBLIC_KEY__#$PUBLIC_KEY#" $TEMP_DIR/workdir/seed/user-data
+
+genisoimage -output $TEMP_DIR/workdir/seed.iso -volid CIDATA -joliet -rock $TEMP_DIR/workdir/seed
+
+rm -rf $TEMP_DIR/workdir/seed
+
+# mount the ubuntu iso to get access to vmlinuz and initrd
+# these two object files allow to add kernel arguments (autoinstall)
+# initrd is the initial RAM layout loaded into -- you guessed it -- the RAM :)
+# vmlinuz is the kernel
+sudo mount -o loop $ISO_PATH $TEMP_DIR/workdir/iso-mount
+cp $TEMP_DIR/workdir/iso-mount/casper/vmlinuz $TEMP_DIR/workdir/iso-copies/
+cp $TEMP_DIR/workdir/iso-mount/casper/initrd $TEMP_DIR/workdir/iso-copies/
+sudo umount $TEMP_DIR/workdir/iso-mount
+
+rm -rf $TEMP_DIR/workdir/iso-mount
+
+# create the qemu image
+qemu-img create -f qcow2 $TEMP_DIR/temp-image.qcow2 $IMAGE_SIZE
+
+# run the install script
+qemu-system-x86_64 \
+	-m 8G \
+	-cpu host \
+	-enable-kvm \
+	-smp 4 \
+	-boot menu=off \
+	-boot order=c \
+	-drive file=$TEMP_DIR/temp-image.qcow2,format=qcow2,cache=none,if=virtio \
+	-drive file=$TEMP_DIR/workdir/seed.iso,format=raw,cache=none,if=virtio \
+	-cdrom $ISO_PATH \
+	-kernel $TEMP_DIR/workdir/iso-copies/vmlinuz \
+	-initrd $TEMP_DIR/workdir/iso-copies/initrd \
+	-append "autoinstall"
+
+mv $TEMP_DIR/temp-image.qcow2 $IMAGE_OUT
+
+rm -rf $TEMP_DIR
